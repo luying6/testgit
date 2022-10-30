9037a7ca4143b4724de239936c164536131f6ba4
luying6 <luyingpool@foxmail.com>
2022-10-30 18:01:31 +0800

    feat: 提交测试1

--- app/src/main/java/com/freebeat/testgit/MainActivity.kt
+++ app/src/main/java/com/freebeat/testgit/MainActivity.kt
@@ -23,6 +23,8 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+
+
         setSupportActionBar(binding.toolbar)
 
         val navController = findNavController(R.id.nav_host_fragment_content_main)
